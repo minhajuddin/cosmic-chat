867eb41c10e60f71f35065774f8b182da01e4562
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-12-20 20:56:41 +0530

    adding test code

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
+require('connect-redis').extend(express)
 
 var app = module.exports = express.createServer();
 
@@ -14,6 +15,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
@@ -21,6 +23,7 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.logger()); 
 });
 
 app.configure('production', function(){
@@ -29,7 +32,28 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/', function(req, res, next){
+  if(Math.random() > 0.5)
+    next();
+  res.render('index', { title: 'Express' })
+});
+app.get('/', function(req, res){
+  res.send('awesome fallback')
+});
+app.get('/user/:id', function(req, res){
+  res.send('user'+ req.params.id)
+});
+
+app.post('/', function(req, res){
+  console.log(req.body.username)
+  res.send(req.body);
+});
+
+app.put('/', function(req, res){
+    console.log('putting stuff')
+  res.send(req.body);
+});
+
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,11 @@
  */
 
 exports.index = function(req, res){
+  if(Math.random() > 0.5)
+    next();
   res.render('index', { title: 'Express' })
+};
+
+exports.test = function(req, res){
+  res.render('index', { title: 'Awesome test' })
 };
